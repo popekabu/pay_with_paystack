e15d6006bfa28f3d028e55593bf7fde961888bc1
Daniel Kabu Asare <popekabu@gmail.com>
2025-05-11 20:39:53 +0000

    remove commented-out AppBar code after implementing Cancel Payment functionality

--- lib/src/paystack_pay_now.dart
+++ lib/src/paystack_pay_now.dart
@@ -197,21 +197,21 @@ class _PaystackPayNowState extends State<PaystackPayNow> {
                 )
                 ..loadRequest(Uri.parse(snapshot.data!.authUrl));
               return Scaffold(
-                appBar: AppBar(
-                  automaticallyImplyLeading: false,
-                  //TODO -> Now that the Cancel Payment works, you can remove this cancel icon.
-                  actions: [
-                    InkWell(
-                        onTap: () async {
-                          await _checkTransactionStatus(
-                                  snapshot.data!.reference)
-                              .then((value) {
-                            Navigator.of(context).pop();
-                          });
-                        },
-                        child: const Icon(Icons.close)),
-                  ],
-                ),
+                // appBar: AppBar(
+                //   automaticallyImplyLeading: false,
+                //   //TODO -> Now that the Cancel Payment works, you can remove this cancel icon.
+                //   actions: [
+                //     InkWell(
+                //         onTap: () async {
+                //           await _checkTransactionStatus(
+                //                   snapshot.data!.reference)
+                //               .then((value) {
+                //             Navigator.of(context).pop();
+                //           });
+                //         },
+                //         child: const Icon(Icons.close)),
+                //   ],
+                // ),
                 body: WebViewWidget(
                   controller: controller,
                 ),
